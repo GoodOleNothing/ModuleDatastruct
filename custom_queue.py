15878d752f59a6382136238902522890b2d4eb69
GoodOleNothing <alexgog77777@gmail.com>
2023-03-08 16:39:22 +0500

    fixed method queue

--- custom_queue.py
+++ custom_queue.py
@@ -8,12 +8,21 @@ class Queue:
     def __init__(self, head=None, tail=None):
         self.head = head
         self.tail = tail
-        self.data_list = []
+    #    self.data_list = []
+#
+    #def enqueue(self, data):
+    #    self.data_list.append(data)
+    #    self.head = Node(self.data_list[0], self.tail)
+    #    self.tail = Node(self.data_list[len(self.data_list) - 1])
 
     def enqueue(self, data):
-        self.data_list.append(data)
-        self.head = Node(self.data_list[0], self.tail)
-        self.tail = Node(self.data_list[len(self.data_list) - 1])
+        if self.tail is None and self.head is None:
+            self.tail = self.head = Node(data, None)
+            return
+
+        self.tail.next_node = Node(data, None)
+        self.tail = self.tail.next_node
+        return
 
 
 queue = Queue()
@@ -24,6 +33,6 @@ print(queue.head.data)
 print(queue.head.next_node.data)
 print(queue.tail.data)
 print(queue.tail.next_node)
-print(queue.tail.next_node.data)
+#print(queue.tail.next_node.data)
 
 
