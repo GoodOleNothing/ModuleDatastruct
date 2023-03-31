f2c3afc971d419351200c37479703415e0c6c255
GoodOleNothing <alexgog77777@gmail.com>
2023-03-31 06:53:27 +0500

    added to_list and get_data_by_id methods, + tests

--- custom_queue.py
+++ custom_queue.py
@@ -32,18 +32,18 @@ class Queue(Node):
 
 
 
-queue = Queue()
-queue.enqueue('data1')
-queue.enqueue('data2')
-queue.enqueue('data3')
-print(queue.head.data)
-print(queue.head.next_node.data)
-print(queue.tail.data)
-print(queue.tail.next_node)
-#print(queue.tail.next_node.data)
-print(queue.dequeue())
-print(queue.dequeue())
-print(queue.dequeue())
-print(queue.dequeue())
+#queue = Queue()
+#queue.enqueue('data1')
+#queue.enqueue('data2')
+#queue.enqueue('data3')
+#print(queue.head.data)
+#print(queue.head.next_node.data)
+#print(queue.tail.data)
+#print(queue.tail.next_node)
+##print(queue.tail.next_node.data)
+#print(queue.dequeue())
+#print(queue.dequeue())
+#print(queue.dequeue())
+#print(queue.dequeue())
 
 
